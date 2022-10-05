f80c0680a3bac2f850b2bce17c0da67d1f9ea08d
Muhammad Enamul Haque <muhammad.enamul.haque79@gmail.com>
2022-10-05 12:14:04 +0600

    dynamic route setup

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Service.css';
 
 
 const Service = (props) => {
-    const{img,name,price,description}=props.service;
+    const{id, img,name,price,description}=props.service;
+    const navigate=useNavigate();
+
+    const navigateToServiceDetail=(id)=>{
+            navigate(`/service/${id}`);
+    };
     return (
         <div className='service'>
             <img src={img} alt="" />
             <h2>{name}</h2>
            <h3> $ {price}</h3>
            <p><small>{description}</small></p>
-           <button className='btn btn-primary'>Book : {name}</button>
+           <button onClick={()=>navigateToServiceDetail(id)} className='btn btn-primary'>Book : {name}</button>
         </div>
     );
 };
